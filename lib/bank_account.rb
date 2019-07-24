7e0aef963c90a620e9132aba55c9e429c325d761
Nathan Rathe <nprathe10@gmail.com>
2019-07-24 15:50:36 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,11 @@
 class BankAccount
 
+  attr_accessor :bank_account, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @bank_account = 1000
+    @status = "open"
+  end
 end
