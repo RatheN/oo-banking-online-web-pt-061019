e9c53a21bf2d405bd9d57e8f1d01cdb5862e24aa
Nathan Rathe <nprathe10@gmail.com>
2019-07-24 15:51:26 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,11 @@
 class BankAccount
 
-  attr_accessor :bank_account, :status
+  attr_accessor :balance, :status
   attr_reader :name
 
   def initialize(name)
     @name = name
-    @bank_account = 1000
+    @balance = 1000
     @status = "open"
   end
 end
